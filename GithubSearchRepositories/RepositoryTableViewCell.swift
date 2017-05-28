37856ce225e698d02b76eba103d0ba2d344339ae
Zoran Todorovic <zoran@arsfutura.co>
2017-05-28 23:02:18 +0200

    Binding repository to tableview.

---
+++ GithubSearchRepositories/RepositoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RepositoryTableViewCell.swift
+//  GithubSearchRepositories
+//
+//  Created by Zoran Todorovic on 28/05/2017.
+//  Copyright © 2017 Zoran Todorovic. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
